3f77fbe5011bec851185c5ae00d5735dfd510cc4
Thierry Hundt <thierry.hundt@students.fhnw.ch>
2019-12-03 15:51:50 +0100

    Added dynamic path

--- src/main/java/ch/fhnw/dist/BloomFilter.java
+++ src/main/java/ch/fhnw/dist/BloomFilter.java
@@ -1,19 +1,24 @@
 package ch.fhnw.dist;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class BloomFilter {
     public static void main(String[] args) {
 
         System.out.println("Start Test");
         FileImport fi = new FileImport();
-        try{
-            ArrayList<String> x = fi.readFileContentToList("C:\\Users\\thier\\OneDrive\\Dokumente\\fhnw\\dist\\Übungen\\Programmieraufgabe2\\words.txt",true);
-            for (String s: x) {
+        try {
+            System.out.println("Insert path to words file");
+            Scanner in = new Scanner(System.in);
+            String path = in. nextLine();
+            ArrayList<String> x = fi.readFileContentToList(path, true);
+            for (String s : x) {
                 System.out.println("homas is " + s);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("homas");
         }
     }
